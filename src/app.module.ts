dafcad9c639bda33d722f84f092a924f21911221
allen456 <alen456@proton.me>
2023-02-14 19:52:09 +0800

    updated function location

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
-import { KafkaModule } from './kafka/kafka.module';
+// import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: `process.env` }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     BlogsModule,
-    KafkaModule
+    // KafkaModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { ProducerService } from 'src/kafka/producer.service';
+// import { ProducerService } from 'src/kafka/producer.service';
 import { Blog, BlogDocument } from './schema/blog.schema';
 
 @Injectable()
 export class BlogsService {
   constructor(
     @InjectModel(Blog.name) private readonly dataModel: Model<BlogDocument>,
-    private readonly producerService: ProducerService
+    // private readonly producerService: ProducerService
     ) {
   }
 
   async create(createBlogDto: Blog) {
     createBlogDto.BlogDate = new Date();
-    this.producerService.produce({
-      topic: process.env.KAFKA_TOPIC,
-      messages: [{
-        value: Buffer.from(JSON.stringify({status: 'create', data: createBlogDto}))
-      }]
-    });
+    // this.producerService.produce({
+    //   topic: process.env.KAFKA_TOPIC,
+    //   messages: [{
+    //     value: Buffer.from(JSON.stringify({status: 'create', data: createBlogDto}))
+    //   }]
+    // });
     return this.dataModel.create(createBlogDto);
   }
 
@@ -33,22 +33,22 @@ export class BlogsService {
 
   update(id: string, updateBlogDto: Blog) {
     updateBlogDto.BlogDate = new Date();
-    this.producerService.produce({
-      topic: process.env.KAFKA_TOPIC,
-      messages: [{
-        value: Buffer.from(JSON.stringify({status: 'update', data: updateBlogDto}))
-      }]
-    });
+    // this.producerService.produce({
+    //   topic: process.env.KAFKA_TOPIC,
+    //   messages: [{
+    //     value: Buffer.from(JSON.stringify({status: 'update', data: updateBlogDto}))
+    //   }]
+    // });
     return this.dataModel.findByIdAndUpdate({ _id: id }, updateBlogDto).exec();
   }
 
   remove(id: string) {
-    this.producerService.produce({
-      topic: process.env.KAFKA_TOPIC,
-      messages: [{
-        value: Buffer.from(JSON.stringify({status: 'delete', data: id}))
-      }]
-    });
+    // this.producerService.produce({
+    //   topic: process.env.KAFKA_TOPIC,
+    //   messages: [{
+    //     value: Buffer.from(JSON.stringify({status: 'delete', data: id}))
+    //   }]
+    // });
     return this.dataModel.findByIdAndRemove({ _id: id }).exec();
   }
 }
